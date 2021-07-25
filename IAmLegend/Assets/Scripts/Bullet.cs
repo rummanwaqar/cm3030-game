d88bfafa91fe9590eb4c31c95d20e370c87a7bdf
Rumman Waqar <rumman.waqar05@gmail.com>
2021-07-25 16:14:49 -0600

    Added shooting

---
+++ IAmLegend/Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletForce = 20f;
+    public float maxTimeAlive = 2f;
+
+    private float _startTime;
+    
+    private void Start()
+    {
+        _startTime = Time.fixedTime;
+        var rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * bulletForce, ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+        if ((_startTime + maxTimeAlive) < Time.fixedTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+}
