b05c9ed9f9c3bd856bd658cf6a69a85fe62f6bbe
ndvo <nelson@ocastudios.com>
2021-08-08 22:27:29 -0300

    fix(shooting): avoid shooting when there is no weapon

--- IAmLegend/Assets/Scripts/ShootingController.cs
+++ IAmLegend/Assets/Scripts/ShootingController.cs
@@ -11,14 +11,17 @@ public class ShootingController : MonoBehaviour
     private float _lastShotTime = 0;
     private Animator _animator;
     private static readonly int Shoot = Animator.StringToHash("shoot");
+    private PlayerCharacterBehaviour _characterBehaviour;
 
     void Start()
     {
         this._animator = GetComponent<Animator>();
+        this._characterBehaviour = GetComponent<PlayerCharacterBehaviour>();
     }
 
     private void Update()
     {
+        if (!this._characterBehaviour.weapon) return;
         if (Input.GetAxisRaw("Fire1") != 0f)
         {
             _shoot();
