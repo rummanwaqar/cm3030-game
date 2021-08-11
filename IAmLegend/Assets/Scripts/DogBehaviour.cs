f0ef9317743ba39051d86efe4f26ee3482bc1c20
Dan <da208@student.london.ac.uk>
2021-08-11 22:29:54 +0300

    Enemies attack and receive damage
    With health bars!

--- IAmLegend/Assets/Scripts/DogBehaviour.cs
+++ IAmLegend/Assets/Scripts/DogBehaviour.cs
@@ -15,68 +15,75 @@ public class DogBehaviour : MonoBehaviour
     private static readonly int IsHit = Animator.StringToHash(("isHit"));
     private static readonly int IsDead = Animator.StringToHash(("isDead"));
     
-    private Animator _animator;
-    private HashSet<DogState> _state;
+    private Animator animator;
+    private NavMeshAgent agent;
+    private HashSet<DogState> state;
 
-    private Vector3 _posPlayer;
-    private Vector3 _posDog;
-    private NavMeshAgent _agent;
+    private Vector3 posPlayer;
+    private Vector3 posDog;
+
+    // Distance between the player and the dog which the dog will go up and follow him
+    [SerializeField] private float radiusToFollow;
+    // Distance between the player and the dog which the dog will sit
+    [SerializeField] private float radiusToSit;
+    // Distance between the player and the enemies to attack
+    [SerializeField] private float radiusToAttack;
 
     // Start is called before the first frame update
     void Start()
     {
-        this._animator = GetComponent<Animator>();
-        this._state = new HashSet<DogState> { };
-        this._agent = GetComponent<NavMeshAgent>();
+        this.animator = GetComponent<Animator>();
+        this.state = new HashSet<DogState> { };
+        this.agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this._setState();
-        this._move();
+        this.SetState();
+        this.Move();
     }
 
-    private void _setState()
+    private void SetState()
     {
         // Get the current position of the player and the dog
-        _posPlayer = followTarget.transform.position;
-        _posDog = transform.position;
+        posPlayer = followTarget.transform.position;
+        posDog = transform.position;
 
-        // Using the distance formula to calculate each position
-        float distanceX = Mathf.Pow(_posPlayer.x - _posDog.x, 2);
-        float distanceZ = Mathf.Pow(_posPlayer.z - _posDog.z, 2);
+        // Using the distance formula to calculate each axis
+        float distanceX = Mathf.Pow(posPlayer.x - posDog.x, 2);
+        float distanceZ = Mathf.Pow(posPlayer.z - posDog.z, 2);
         float distance = Mathf.Pow(distanceX + distanceZ, 0.5f);
 
-        // If the distance radius is bigger than 5, the dog shall go to his master
-        if( distance >= 5 )
-            this._state.Add(DogState.Walk);
+        // If the distance radius is bigger than 'radiusToIdle', the dog shall go to his master
+        if( distance >= radiusToFollow )
+            this.state.Add(DogState.Walk);
 
         // If the dog is close enough to the player, the dog shall sit
-        if( distance <= 1.5f )
+        if( distance <= radiusToSit )
         {
-            this._state.Add(DogState.nearPlayer);
-            this._state.Remove(DogState.Walk);
+            this.state.Add(DogState.nearPlayer);
+            this.state.Remove(DogState.Walk);
         }
         else
-            this._state.Remove(DogState.nearPlayer);
+            this.state.Remove(DogState.nearPlayer);
     }
 
-    private void _move()
+    private void Move()
     {
         // Update the logic state based on _setState()
-        bool isWalking = this._state.Contains(DogState.Walk);
-        bool isNearPlayer = this._state.Contains(DogState.nearPlayer);
+        bool isWalking = this.state.Contains(DogState.Walk);
+        bool isNearPlayer = this.state.Contains(DogState.nearPlayer);
 
         if( isWalking )
         {
             // Pathfinding algorithm (the terrain must be baked first)
             Vector3 offset = new Vector3(1f, 0, 1f);  // to avoid collisions with the player
-            _agent.SetDestination(_posPlayer - offset);
+            agent.SetDestination(posPlayer - offset);
         }
 
         // Update animations
-        this._animator.SetBool(IsWalking, isWalking);
-        this._animator.SetBool(IsNearPlayer, isNearPlayer);
+        this.animator.SetBool(IsWalking, isWalking);
+        this.animator.SetBool(IsNearPlayer, isNearPlayer);
     }
 }
--- IAmLegend/Assets/Scripts/EnemyBehaviour.cs
+++ IAmLegend/Assets/Scripts/EnemyBehaviour.cs
@@ -2,26 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    private enum EnemyState { Idle, ChaseTarget, AttackTarget }
-   
+    private enum EnemyState { Idle, ChaseTarget, AttackTarget, GotHit, Dead }
+
+    // AI & navigation
     [SerializeField] private float distanceSight;
-    [SerializeField] private float angleView; // both left & right
+    [SerializeField] private float distanceAttack;
+    [SerializeField] private float angleView;
     [SerializeField] private LayerMask targetsLayers;
     [SerializeField] private LayerMask obstaclesLayers;
-
-    private EnemyState enemyCurrentState;
     private Collider detectedTarget;
     private NavMeshAgent agent;
+
+    // Health & damage
+    [SerializeField] private float bulletDamage;
+    [SerializeField] private float deathAnimationTime;
+    [SerializeField] private float health;
+    [SerializeField] private Slider healthbar;
+    
+    // Animation
+    private EnemyState enemyCurrentState;
     private Animator animator;
 
     // Start is called before the first frame update
     void Awake()
     {
-        this.agent = GetComponentInParent<NavMeshAgent>();
-        this.animator = GetComponentInParent<Animator>();
+        this.agent = GetComponent<NavMeshAgent>();
+        this.animator = GetComponent<Animator>();
         this.enemyCurrentState = EnemyState.Idle;    // Enemy is idle on initial spawn
     }
 
@@ -62,22 +72,34 @@ public class EnemyBehaviour : MonoBehaviour
         }
     }
 
+    // Update the enemy's state
     private void UpdateState()
     {
-        if( enemyCurrentState == EnemyState.Idle )
+        if( enemyCurrentState == EnemyState.Dead || health <= 0 )
+            StartCoroutine(Dead());
+        else if( enemyCurrentState == EnemyState.Idle )
             Idle();
         else if( enemyCurrentState == EnemyState.ChaseTarget )
             ChaseTarget();
         else if( enemyCurrentState == EnemyState.AttackTarget )
             AttackTarget();
+        else if( enemyCurrentState == EnemyState.GotHit )
+            ReceiveDamage(50);
     }
 
     private void Idle()
     {
-        // If target was detected, change state
+        // If target was detected, chase him
         if( this.detectedTarget != null )
         {
+            // Target detected, chase him
             this.enemyCurrentState = EnemyState.ChaseTarget;
+            return;
+        }
+        else if( this.health <= 0 )
+        {
+            // No health, die
+            this.enemyCurrentState = EnemyState.Dead;
             return;
         }
 
@@ -87,41 +109,108 @@ public class EnemyBehaviour : MonoBehaviour
 
     private void ChaseTarget()
     {
-        // If target wasn't detected, change state
+        // If target wasn't detected, be idle
         if( this.detectedTarget == null )
         {
             this.enemyCurrentState = EnemyState.Idle;
             return;
         }
-
-        this.agent.isStopped = false;
-        this.animator.SetTrigger("Walking");
+        else if( this.health <= 0 )
+        {
+            // No health, die
+            this.enemyCurrentState = EnemyState.Dead;
+            return;
+        }
 
         // Calculate the distance to the target
         float distanceToTarget = Vector3.Distance(this.transform.position, this.detectedTarget.transform.position);
         this.agent.SetDestination(this.detectedTarget.transform.position);
-        
-        // TODO: if near target -> attack.
+
+        // Attack the near targets
+        if( Mathf.Abs(distanceToTarget) < distanceAttack )
+            this.AttackTarget();
+        else
+        {
+            // Chase the target
+            this.agent.isStopped = false;
+            this.animator.SetTrigger("Walking");
+        }
     }
 
     private void AttackTarget()
     {
-        // If target wasn't detected, change state
+        // If target wasn't detected, be idle
         if( this.detectedTarget == null )
         {
             this.enemyCurrentState = EnemyState.Idle;
             return;
         }
-        // else if(target runs) -> chase him
+        else if( this.health <= 0 )
+        {
+            // No health, die
+            this.enemyCurrentState = EnemyState.Dead;
+            return;
+        }
 
         this.agent.isStopped = true;
         this.animator.SetTrigger("Attacking");
+
+        // If the target is close, deal damange
+        DealDamage();
     }
 
-    //// Debugging variables with visual gizmos
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawSphere(transform.position, distanceSight);
-    //}
+    private IEnumerator Dead()
+    {
+        this.agent.isStopped = true;
+
+        // Play death animation manually
+        animator.Play("zombie_death_standing");
+
+        // Wait for the death animation to finish
+        yield return new WaitForSeconds(deathAnimationTime);
+
+        // Delete the zombie from the scene
+        Destroy(gameObject);
+    }
+
+    // The zombie was attacked successfully
+    private void ReceiveDamage( float damage )
+    {
+        
+        health -= damage;           // Update health
+        health /= 100;              // Normalize the value
+        healthbar.value = health;
+    }
+
+    private void DealDamage()
+    {
+        // Call external functions:
+        // Target.damage()
+    }
+
+    // Collision with tag bullets/ammo receive damage
+    void OnTriggerEnter( Collider other )
+    {
+        switch( other.tag )
+        {
+            case "Bullet":
+                ReceiveDamage(bulletDamage);
+                break;
+            default:
+                ReceiveDamage(0);
+                break;
+        }
+    }
+
+    //#if UNITY_EDITOR
+    //    // visual gizmos
+    //    private void OnDrawGizmos()
+    //    {
+    //        {
+    //            // Display how far the zombie can see
+    //            Gizmos.color = Color.red;
+    //            Gizmos.DrawSphere(transform.position, distanceSight);
+    //        }
+    //    }
+    //#endif
 }
